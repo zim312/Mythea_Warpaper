4118fcefce26ee3cd1e2aca23b3d034476c2c22a
Aaron <github@mailmehere.com>
2020-07-19 02:01:56 -0500

    Create chatfix.js

---
+++ chatfix.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Mythea Chatfix
+// @namespace    http://mythea.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Zim/Shiver
+// @match        http://www.mythea.com/
+// @grant        none
+// ==/UserScript==
+
+chaturl = "http://cs.etmc.org/chat/chat9371";
